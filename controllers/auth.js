81e9e0e4f8cc2406155f7fe1c497706c93d7a509
oscar-quiroz <kairox.rt@gmail.com>
2021-07-30 13:25:45 -0500

    validaciones con jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,98 @@
 const { response } = require("express");
 const { validationResult } = require("express-validator");
+const Usuario = require("../models/Usuario");
+const bcrypt = require('bcryptjs')
+const { generarJWT } = require('../helpers/jwt')
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async(req, res = response) => {
     const { email, name, password } = req.body;
-    console.log(email, name, password);
+
+    try {
+        //verificar si noe xiste correo igual
+        const usuario = await Usuario.findOne({ email });
+
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: "El usuario ya existe con ese email",
+            });
+        }
+
+        //crear ususario con el modelo
+        const dbUser = new Usuario(req.body);
+
+        //encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        dbUser.password = bcrypt.hashSync(password, salt);
+
+        //Generar el JWT como metodo de autenticacion
+        const token = await generarJWT(dbUser.id, name)
+            //crear usuario de base de datos
+
+        await dbUser.save();
+
+        //generar respuesta exitosa.
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name,
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: "por favor hable con el admin",
+        });
+    }
+
     return res.json({
         ok: true,
         mesg: "crear un nuevo usuario  /new",
     });
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async(req, res = response) => {
     const { email, password } = req.body;
-    console.log(email, password);
-    return res.json({
-        ok: true,
-        mesg: "Login de usuario  /",
-    });
+
+    //
+    try {
+
+        const dbUser = await Usuario.findOne({ email });
+
+        if (!dbUser) {
+            return res.status(400).json({
+                ok_false,
+                msg: 'El correo no existe'
+            });
+        }
+
+        const validPassword = bcrypt.compareSync(password, dbUser.password)
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: "EL pásword no es valido"
+
+            })
+        }
+
+        const token = await generarJWT(dbUser.id, dbUser.name)
+
+        returnres.json({
+            ok: true,
+            uid: dbUser.id,
+            name,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'hable con el administrador'
+        });
+    }
 };
 
 //kshadlkhasdklhasklhdasl
